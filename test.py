4d0e2f33f3901edfe29b68532b49afe119a203df
t6fg78q9jio <83807358+t6fg78q9jio@users.noreply.github.com>
2021-05-16 17:14:51 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+nodemon --ext py,js,mjs,ts app.mjs 
+"""
+
+import math
+import emoji
+import sys
+import socket
+import time
+import os
+import re
+import types
+import loguru
+from rich import print
+import requests
+
+print("OK")
